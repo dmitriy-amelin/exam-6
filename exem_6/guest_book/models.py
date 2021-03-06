d4137428b5905ebcc1eee1574fcf7978dbf43bca
Reey <trommxxx@mail.ru>
2021-03-06 11:55:38 +0600

    Создана модель GuestBook

--- exem_6/guest_book/models.py
+++ exem_6/guest_book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [("active", "Активно"), ("blocked", "Заблокировано")]
+
+
+class GuestBook(models.Model):
+    author = models.CharField(max_length=40, verbose_name='Автор')
+    email = models.EmailField(max_length=254, verbose_name='Почта')
+    text = models.TextField(verbose_name='Текст записи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
+    status = models.CharField(max_length=40, choices=status_choices, default='active', verbose_name='Статус')
+
+    class Meta:
+        db_table = 'guest_book'
+        verbose_name = 'Гостевая книга'
+        verbose_name_plural = 'Гостевая книга'
+
+    def __str__(self):
+        return f'{self.author}: {self.text}, {self.status}'
