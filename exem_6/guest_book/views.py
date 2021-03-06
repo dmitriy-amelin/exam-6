90b200c7d06f3d78e59a9f4eb78c9c4cc009d137
Reey <trommxxx@mail.ru>
2021-03-06 14:39:36 +0600

    В представлении для главной страницы добавлен фильт и сортировка

--- exem_6/guest_book/views.py
+++ exem_6/guest_book/views.py
@@ -3,7 +3,6 @@ from guest_book.models import GuestBook
 
 
 def index_view(request):
-    guest_book = GuestBook.objects.all()
+    guest_book = GuestBook.objects.filter(status="active").order_by('-created_at')
 
     return render(request, 'index.html', {'guest_book': guest_book})
-
