adf937693945f5537cb42ea9668d73dc41f5cb32
Reey <trommxxx@mail.ru>
2021-03-06 15:00:26 +0600

    Добавлено представление для добавления отзыва метод GET

--- exem_6/guest_book/views.py
+++ exem_6/guest_book/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from guest_book.models import GuestBook
+from guest_book.forms import ReviewForm
+from guest_book.models import GuestBook, status_choices
 
 
 def index_view(request):
     guest_book = GuestBook.objects.filter(status="active").order_by('-created_at')
 
     return render(request, 'index.html', {'guest_book': guest_book})
+
+
+def add_review(request):
+    if request.method == 'GET':
+        form = ReviewForm()
+        return render(request, 'add_review.html', context={'form': form, 'choices': status_choices})
