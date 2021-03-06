5d1a50849cf70227240166df0382ac5279e5bfa3
Reey <trommxxx@mail.ru>
2021-03-06 12:55:39 +0600

    Изменение вывода полей в админке для создания и применение к полям readonly_fields чтобы отображать поля без возможности их редактирования

--- exem_6/guest_book/admin.py
+++ exem_6/guest_book/admin.py
@@ -3,10 +3,11 @@ from guest_book.models import GuestBook
 
 
 class GuestBookAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author', 'email', 'text', 'status', 'created_at', 'updated_at', 'status']
+    readonly_fields = ('author', 'email', 'text')
+    list_display = ['author', 'email', 'text', 'created_at', 'updated_at', 'status']
     list_filter = ['status', 'created_at']
     search_fields = ['status', 'created_at']
-    fields = ['id', 'author', 'email', 'text', 'created_at', 'updated_at', 'status']
+    fields = ['author', 'email', 'text', 'status']
 
 
 admin.site.register(GuestBook, GuestBookAdmin)
