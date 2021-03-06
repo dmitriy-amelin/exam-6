24f94e9645b9884bb4a07f16cbf8c02cb7104504
Reey <trommxxx@mail.ru>
2021-03-06 14:38:30 +0600

    Изменена модель обновлены фикстуры

--- exem_6/guest_book/models.py
+++ exem_6/guest_book/models.py
@@ -8,7 +8,7 @@ class GuestBook(models.Model):
     email = models.EmailField(max_length=254, verbose_name='Почта')
     text = models.TextField(verbose_name='Текст записи')
     created_at = models.DateField(auto_now_add=True, verbose_name='Время создания')
-    updated_at = models.DateField(auto_now=True, verbose_name='Время редактирования')
+    updated_at = models.DateField(null=True, blank=True, verbose_name='Время редактирования')
     status = models.CharField(max_length=40, choices=status_choices, default='active', verbose_name='Статус')
 
     class Meta:
