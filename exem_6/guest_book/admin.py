1a4ad9f43fad55aa907fe77ada434b52cf9a2554
Reey <trommxxx@mail.ru>
2021-03-06 12:17:33 +0600

    Создана class GuestBookAdmin для регистрации модели в админке

--- exem_6/guest_book/admin.py
+++ exem_6/guest_book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from guest_book.models import GuestBook
 
-# Register your models here.
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'text', 'status', 'created_at', 'updated_at', 'status']
+    list_filter = ['status', 'created_at']
+    search_fields = ['status', 'created_at']
+    fields = ['id', 'author', 'email', 'text', 'created_at', 'updated_at', 'status']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
