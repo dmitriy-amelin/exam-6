a0f535a48f92d9c9e01c46480f45fb4e1f82af7d
Reey <trommxxx@mail.ru>
2021-03-06 16:15:37 +0600

    Добавлен шаблон детального просмотра отзыва

--- exem_6/guest_book/urls.py
+++ exem_6/guest_book/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from guest_book.views import (
     index_view,
     add_review,
+    review_view,
+    review_update_view,
 
 )
 
 urlpatterns = [
     path('', index_view, name='review-list'),
     path('review/add/', add_review, name='add_review'),
+    path('review/<int:pk>', review_view, name='review-view'),
+    path('<int:pk>/update', review_update_view, name='review-update'),
 
 ]
