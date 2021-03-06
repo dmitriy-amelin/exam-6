bffb2ac8f35d6021c6a914bb50e9262c24ee98a1
Reey <trommxxx@mail.ru>
2021-03-06 16:14:45 +0600

    В представление для создания отзыва добавлен метод POST

--- exem_6/guest_book/views.py
+++ exem_6/guest_book/views.py
@@ -13,3 +13,22 @@ def add_review(request):
     if request.method == 'GET':
         form = ReviewForm()
         return render(request, 'add_review.html', context={'form': form, 'choices': status_choices})
+    elif request.method == 'POST':
+        form = ReviewForm(data=request.POST)
+        if form.is_valid():
+            review = GuestBook.objects.create(
+                author=form.cleaned_data.get("author"),
+                email=form.cleaned_data.get("email"),
+                text=form.cleaned_data.get("text")
+            )
+            return redirect('review-view', pk=review.id)
+        return render(request, 'add_review.html', context={'form': form, 'choices': status_choices})
+
+
+def review_view(request, pk):
+    review = get_object_or_404(GuestBook, id=pk)
+    return render(request, 'review-view.html', context={'review': review})
+
+
+def review_update_view(request, pk):
+    pass
