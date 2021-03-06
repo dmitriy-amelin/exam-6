07720f3ea86eb14ac077b97dd324723ab851b531
Reey <trommxxx@mail.ru>
2021-03-06 15:00:55 +0600

    Добавлено путь для добавления отзыва

--- exem_6/guest_book/urls.py
+++ exem_6/guest_book/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from guest_book.views import (
     index_view,
+    add_review,
 
 )
 
 urlpatterns = [
     path('', index_view, name='review-list'),
+    path('review/add/', add_review, name='add_review'),
 
 ]
