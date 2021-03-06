79cc25024459574e91bad32801634e6bf379fd7e
Reey <trommxxx@mail.ru>
2021-03-06 14:42:40 +0600

    В админке заблокировано изменение редактирования полей

--- exem_6/guest_book/admin.py
+++ exem_6/guest_book/admin.py
@@ -7,7 +7,7 @@ class GuestBookAdmin(admin.ModelAdmin):
     list_display = ['author', 'email', 'text', 'created_at', 'updated_at', 'status']
     list_filter = ['status', 'created_at']
     search_fields = ['status', 'created_at']
-    fields = ['author', 'email', 'text', 'status']
+    fields = ['author', 'email', 'text', 'status', 'updated_at']
 
 
 admin.site.register(GuestBook, GuestBookAdmin)
